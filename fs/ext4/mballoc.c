e83f2665a9d4f4b54b3e8572c63a23f2a6c49349
Ulrich Hecht <uli@kernel.org>
2026-01-29 12:48:14 +0800

    tree: import: Merge 'linux-4.19.st12'
    upstream to 4.19.341
    
    Brings the tree up-to-date with 5.10.248.
    
     Conflicts:
           drivers/rpmsg/qcom_glink_native.c
           drivers/usb/host/xhci-hub.c
           fs/ext4/mballoc.c
           fs/f2fs/file.c
           fs/f2fs/super.c
    
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    Signed-off-by: jo <@JO_0012>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -558,7 +558,7 @@ do {									\
  *    - Verify that pa_pstart falls within the bounds of this block group.
  *    - Ensure the corresponding bit(s) in the order-0 bitmap are marked as allocated (1).
  */
-static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
+static void __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 				const char *function, int line)
 {
 	struct super_block *sb = e4b->bd_sb;
@@ -579,7 +579,7 @@ static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 	{
 		static int mb_check_counter;
 		if (mb_check_counter++ % 100 != 0)
-			return 0;
+			return;
 	}
 
 	while (order > 1) {
@@ -645,12 +645,13 @@ static int __mb_check_buddy(struct ext4_buddy *e4b, char *file,
 		ext4_group_t groupnr;
 		struct ext4_prealloc_space *pa;
 		pa = list_entry(cur, struct ext4_prealloc_space, pa_group_list);
+		if (!pa->pa_len)
+			continue;
 		ext4_get_group_no_and_offset(sb, pa->pa_pstart, &groupnr, &k);
 		MB_CHECK_ASSERT(groupnr == e4b->bd_group);
 		for (i = 0; i < pa->pa_len; i++)
 			MB_CHECK_ASSERT(mb_test_bit(k + i, buddy));
 	}
-	return 0;
 }
 #undef MB_CHECK_ASSERT
 #define mb_check_buddy(e4b) __mb_check_buddy(e4b,	\
